9d58d9bfe473d504ee69bdd88e7a3a25c4a6ddd6
Sheshasai Dyavanapally <Sheshasai>
2025-10-05 20:51:54 +0530

    PERSON1, PERSON2

---
+++ 03-conversation.sh
@@ -0,0 +1,10 @@
+ #!/bin/bash
+
+ PERSON1=John
+ PERSON2=Nani
+
+ echo "$PERSON1: Hi $PERSON2, How are you"
+ echo "$PERSON2: Hi $PERSON1, I am good, How are you."
+ echo "$PERSON1: What are you doing."
+ echo "$PERSON2: I am going to office."
+ echo "$PERSON1: Nice Good..!!"   
