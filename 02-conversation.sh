787faa4234886a5e535a540af107445e5d4bafd5
Sheshasai Dyavanapally <Sheshasai>
2025-10-05 19:36:51 +0530

    Ramesh, suresh

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+ #!/bin/bash
+
+ echo "Suresh: Hi Ramesh, How are you"
+ echo "Ramesh: Hi Suresh, I am good, How are you."
+ echo "Suresh: What are you doing."
+ echo "Ramesh: I am going to office."
