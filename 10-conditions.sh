de95addc27168463315f7bc461569ed575e856d0
Sheshasai Dyavanapally <Sheshasai>
2025-10-06 17:05:37 +0530

    10-conditions.sh

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -3,8 +3,8 @@
 NUMBER=100
 
 if [ $NUMBER lt 100 ]; then
-echo "Given number $NUMBER is less than 100"
+echo "Given number is less than 100"
 else
-echo "Given number $NUMBER is greater than 100"
+echo "Given number is greater than 100"
 fi
 
