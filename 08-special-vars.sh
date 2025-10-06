fe2eedd97ee627ca5e31dcc6adcfcc0435ca46c9
Sheshasai Dyavanapally <Sheshasai>
2025-10-06 16:02:08 +0530

    08-special-vars.sh

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -4,6 +4,6 @@ echo "All names are here : $@"
 echo "All names are here : $*"
 echo "What is the directory : $PWD"
 echo "Who is running this : $HOME"
-echo "What is script name : $0"
+echo "What is script name : $0" 
 
 
