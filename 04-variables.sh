b575596517622c3ecd6aea0a7d0aff59febc3f43
Sheshasai Dyavanapally <Sheshasai>
2025-10-06 11:55:27 +0530

    variables.sh

--- 04-variables.sh
+++ 04-variables.sh
@@ -5,9 +5,9 @@ PERSON1=$1
 PERSON2=$2
 
 
-echo "PERSON1:: Welcome to USA"
-echo "PERSON2:: Thank your wishes, how are you"
-echo "PERSON1:: I am good PERSON2, How are you"
-echo "PERSON2:: I am doing well"
-echo "PERSON1:: Let's have a meetin now"
-echo "PERSON2:: Sure!! Will start"
+echo "$PERSON1:: Welcome to USA"
+echo "$PERSON2:: Thank your wishes, how are you"
+echo "$PERSON1:: I am good $PERSON2, How are you"
+echo "$PERSON2:: I am doing well"
+echo "$PERSON1:: Let's have a meetin now"
+echo "$PERSON2:: Sure!! Will start"
