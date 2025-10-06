30e027dea7ff0cf43a4b67d7b920693d80f26f7b
Sheshasai Dyavanapally <Sheshasai>
2025-10-06 18:54:34 +0530

    13-Installations.sh

---
+++ 13-installation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root user"
+    exit 1
+fi
+
+VALIDATE(){ }
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing my $2 is failed"
+else
+    echo "Installing $2 is success"
+fi
+
+dnf install nginx -y
+VALIDATE $? "MySQL"
+
+dnf install  nginx -y
+VALIDATE $? "Nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "Mongosh"
+
+
